c51a8879e7968861ca996c16dfe51cfd8ec31a4c
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2019-08-01 20:02:27 -0400

    Input live data into components from back end

--- src/components/shared/NonProfitList.js
+++ src/components/shared/NonProfitList.js
@@ -2,47 +2,48 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import NonProfit from './NonProfit';
 
-import { orgs } from '../../data.js';
+// import { orgs } from '../../data.js';
 
 function NonProfitList({ userQuery, userZip }) {
     const [nonProfits, setNonProfits] = useState([]);
 
     useEffect(() => {
-        setNonProfits(orgs);
-        console.log(userQuery);
+        let search = `description_text=${userQuery}`;
 
-        axios.get('https://projects.propublica.org/nonprofits/api/v2/search.json').then(res => {
-            console.log(res);
-        });
+        axios
+            .post('//50.62.56.186:8000/find', search, {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    Accept: 'text/html; charset=utf-8'
+                }
+            })
+            .then(res => {
+                let arrayified = [];
+
+                for (let key in res.data) {
+                    arrayified.push(res.data[key]);
+                }
+
+                console.log(`Response for the query '${userQuery}' was:`, arrayified);
+
+                setNonProfits(arrayified);
+            })
+            .catch(err => console.log('There was an error:', err));
     }, [userQuery]);
+
+    let searchResults;
+
+    if (!nonProfits.length) {
+        searchResults = 'Non-profits are loading...';
+    } else {
+        searchResults = nonProfits.map((nonProfit, index) => <NonProfit key={index} nonProfit={nonProfit} />);
+    }
 
     return (
         <div className="non-profit-list">
-            <div className="for-testing">
-                <p style={{ fontSize: '1.3rem', color: 'red' }}>For testing purposes only</p>
-                <p style={{ color: 'red', marginBottom: '20px' }}>
-                    The users query was: <span style={{ color: '#000' }}>{userQuery}</span>
-                </p>
-                <p style={{ color: 'red', marginBottom: '20px' }}>
-                    The users zip code was: <span style={{ color: '#000' }}>{userZip}</span>
-                </p>
-            </div>
-            {nonProfits.map(nonProfit => (
-                <NonProfit key={nonProfit.id} nonProfit={nonProfit} />
-            ))}
+            <div className="full-list">{searchResults}</div>
         </div>
     );
-
-    // , {
-    //     headers: {
-    //         'Access-Control-Allow-Origin': '*',
-    //         'Content-Type': 'application/json'
-    //     },
-    //     proxy: {
-    //         host: '66.43.34.50',
-    //         port: 8080
-    //     }
-    // }
 }
 
 export default NonProfitList;
